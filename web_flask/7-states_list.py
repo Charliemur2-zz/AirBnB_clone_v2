71a33679f0e7529defc21fc0a3d88462c530c304
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-02 22:40:51 +0000

    create eb_flask/7-statesist.py web_flask/templates/7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-
 @app.teardown_appcontext
 def app_teardown_appcontext(self):
     """
@@ -22,7 +21,8 @@ def state_list():
     """
     return the render of 7-states_list.html
     """
-    return render_template('7-states_list.html', states=storage.all(State).values())
+    return render_template('7-states_list.html',
+                           states=storage.all(State).values())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
